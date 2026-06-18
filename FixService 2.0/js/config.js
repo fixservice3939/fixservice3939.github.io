22dc3782fa945c8b21078ed0fcb14c3d45dc294e
fixservice3939 <fix.service3939@gmail.com>
2026-06-18 07:36:24 +0300

    Add files via upload

---
+++ FixService 2.0/js/config.js
@@ -0,0 +1,7 @@
+// ============================================================
+// НАСТРОЙКИ БОТА ДЛЯ ОТПРАВКИ ЗАЯВОК В TELEGRAM
+// ============================================================
+
+const BOT_TOKEN = '8676096132:AAE0LDxXgNk4_8n0waZtf-6aiQR0g1-lnx8';
+const CHAT_ID = '6634773779';
+const TELEGRAM_API_URL = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
