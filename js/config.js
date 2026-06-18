ec748034c01039bd4f8602d46cd9bf016ae8bcbd
fixservice3939 <fix.service3939@gmail.com>
2026-06-18 08:06:25 +0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@
 // НАСТРОЙКИ БОТА ДЛЯ ОТПРАВКИ ЗАЯВОК В TELEGRAM
 // ============================================================
 
-const BOT_TOKEN = '8676096132:AAE0LDxXgNk4_8n0waZtf-6aiQR0g1-lnx8';
+const BOT_TOKEN = '8877207809:AAFhA1S8UZfJfpUbNKrGh7Z5ZjQzlwxs2rY';
 const CHAT_ID = '6634773779';
 const TELEGRAM_API_URL = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
