69ba4641c57f753573643b236eac8618cf9502ad
fixservice3939 <fix.service3939@gmail.com>
2026-06-18 08:07:32 +0300

    Delete FixService 2.0 directory

--- FixService 2.0/js/config.js
+++
@@ -1,7 +0,0 @@
-// ============================================================
-// НАСТРОЙКИ БОТА ДЛЯ ОТПРАВКИ ЗАЯВОК В TELEGRAM
-// ============================================================
-
-const BOT_TOKEN = '8676096132:AAE0LDxXgNk4_8n0waZtf-6aiQR0g1-lnx8';
-const CHAT_ID = '6634773779';
-const TELEGRAM_API_URL = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
