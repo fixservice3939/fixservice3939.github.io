7dfeab395d052989ba561fc76a3f97abd9b6f868
fixservice3939 <fix.service3939@gmail.com>
2026-06-20 00:33:46 +0300

    Add files via upload
    изменил сервер

---
+++ send.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json');
+
+$serverUrl = 'http://46.29.165.147:5000/api/send';
+
+$ch = curl_init($serverUrl);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents('php://input'));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+http_response_code($httpCode);
+echo $response;
+?>
